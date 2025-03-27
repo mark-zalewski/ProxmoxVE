3ec8ef2cebc313b3619a7ac8e3e13757f1b5e3e9
mark-zalewski <64677761+mark-zalewski@users.noreply.github.com>
2025-03-27 09:15:41 -0500

    Create erstatztvmark.sh

---
+++ install/erstatztvmark.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 tteck
+# Author: MickLesk (Canbiz)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://ersatztv.org/
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing FFmpeg (Patience)"
+cd /usr/local/bin
+wget -q https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
+$STD tar -xvf ffmpeg-release-amd64-static.tar.xz
+rm -f ffmpeg-*.tar.xz
+cd ffmpeg-*
+mv ffmpeg ffprobe /usr/local/bin/
+rm -rf /usr/local/bin/ffmpeg-*
+msg_ok "Installed FFmpeg"
+
+msg_info "Setting Up Hardware Acceleration"
+$STD apt-get -y install {va-driver-all,ocl-icd-libopencl1,intel-opencl-icd,vainfo,intel-gpu-tools}
+if [[ "$CTTYPE" == "0" ]]; then
+  chgrp video /dev/dri
+  chmod 755 /dev/dri
+  chmod 660 /dev/dri/*
+  $STD adduser $(id -u -n) video
+  $STD adduser $(id -u -n) render
+fi
+msg_ok "Set Up Hardware Acceleration"
+
+msg_info "Installing ErsatzTV"
+cd /opt
+wget -qO- "https://github.com/ErsatzTV/ErsatzTV/releases/download/v25.1.0/ErsatzTV-v25.1.0-linux-x64.tar.gz" -O "$temp_file"
+tar -xzf "$temp_file"
+mv /opt/ErsatzTV-v25.1.0-linux-x64 /opt/ErsatzTV
+echo "v25.1.0" >"/opt/${APPLICATION}_version.txt"
+msg_ok "Installed ErsatzTV"
+
+msg_info "Creating Service"
+cat <<EOF >/etc/systemd/system/ersatzTV.service
+[Unit]
+Description=ErsatzTV Service
+After=multi-user.target
+
+[Service]
+Type=simple
+User=root
+WorkingDirectory=/opt/ErsatzTV 
+ExecStart=/opt/ErsatzTV/ErsatzTV  
+Restart=always
+RestartSec=30
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable -q --now ersatzTV
+msg_ok "Created Service"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+rm -f ${temp_file}
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
